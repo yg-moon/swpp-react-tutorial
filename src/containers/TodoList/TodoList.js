5f92f8d6f40e5ee991d24d02a70a21c342c9a5a6
ktaebum <phya.ktaebum@gmail.com>
2019-09-18 11:44:07 +0900

    todo detail finished

--- src/containers/TodoList/TodoList.js
+++ src/containers/TodoList/TodoList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Todo from '../../components/Todo/Todo';
+import TodoDetail from '../../components/TodoDetail/TodoDetail';
 import './TodoList.css';
 
 class TodoList extends Component {
@@ -10,16 +11,40 @@ class TodoList extends Component {
       { id: 2, title: 'Movie', content: 'watch movie', done: false },
       { id: 3, title: 'Dinner', content: 'eat dinner', done: false }
     ],
+    selectedTodo: null,
+  }
+
+  clickTodoHandler = (td) => {
+    if (this.state.selectedTodo === td) {
+      this.setState({ ...this.state, selectedTodo: null });
+    } else {
+      this.setState({ ...this.state, selectedTodo: td });
+    }
   }
 
   render() {
     const todos = this.state.todos.map((td) => {
-      return (<Todo key={td.id} title={td.title} done={td.done} />);
+      return (
+        <Todo
+          key={td.id}
+          title={td.title}
+          done={td.done}
+          clicked={() => this.clickTodoHandler(td)}
+        />);
     });
+
+    let todo = null;
+    if (this.state.selectedTodo) {
+      todo = <TodoDetail
+        title={this.state.selectedTodo.title}
+        content={this.state.selectedTodo.content}
+      />
+    }
     return (
       <div className="TodoList">
         <div className="title">{this.props.title}</div>
         <div className="todos">{todos}</div>
+        {todo}
       </div>
     )
   }
